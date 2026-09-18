5a1cce8982f83cc953c88b3f421d89c803737d6c
CrabKinea <kattt4996@gmail.com>
2026-09-18 16:13:26 +0700

    Update os_arch_features.h

--- common/os_arch_features.h
+++ common/os_arch_features.h
@@ -1,46 +1,93 @@
 #pragma once
 
-#include <sys/types.h>
-#include <stddef.h>
-#include "pac_kit.h"
+#include <cstdint>
+#include <cstddef>
+#include <cstdarg>
 
-#include "PlatformUnifiedInterface/platform.h"
+namespace base {
 
-#if defined(__arm64e__) && __has_feature(ptrauth_calls)
-#include <ptrauth.h>
-#endif
+class ThreadLocalStorageInterface {
+  using LocalStorageKey = int32_t;
 
-namespace features {
+  static LocalStorageKey CreateThreadLocalKey();
 
-template <typename T> inline T arm_thumb_fix_addr(T &addr) {
-#if defined(__arm__) || defined(__aarch64__)
-  addr = (T)((uintptr_t)addr & ~1);
-#endif
-  return addr;
-}
+  static void DeleteThreadLocalKey(LocalStorageKey key);
 
-namespace apple {
-template <typename T> inline T arm64e_pac_strip(T &addr) {
-  return pac_strip(addr);
-}
+  static void *GetThreadLocal(LocalStorageKey key);
 
-template <typename T> inline T arm64e_pac_sign(T &addr) {
-  return pac_sign(addr);
-}
-
-template <typename T> inline T arm64e_pac_strip_and_sign(T &addr) {
-  return pac_strip_and_sign(addr);
-}
-} // namespace apple
-
-namespace android {
-inline void make_memory_readable(void *address, size_t size) {
-#if defined(ANDROID)
-  auto page = (void *)ALIGN_FLOOR(address, OSMemory::PageSize());
-  if (!OSMemory::SetPermission(page, OSMemory::PageSize(), kReadExecute)) {
-    return;
+  static int GetThreadLocalInt(LocalStorageKey key) {
+    return static_cast<int>(reinterpret_cast<intptr_t>(GetThreadLocal(key)));
   }
-#endif
-}
-} // namespace android
-} // namespace features
+
+  static void SetThreadLocal(LocalStorageKey key, void *value);
+
+  static void SetThreadLocalInt(LocalStorageKey key, int value) {
+    SetThreadLocal(key, reinterpret_cast<void *>(static_cast<intptr_t>(value)));
+  }
+
+  static bool HasThreadLocal(LocalStorageKey key) {
+    return GetThreadLocal(key) != nullptr;
+  }
+};
+
+typedef void *ThreadHandle;
+
+struct ThreadInterface {
+  base::ThreadHandle handle;
+  int id = 0;
+  char name[256] = {0};
+  uint32_t stack_size = 4 * 1024 * 1024;
+
+  struct Delegate {
+    [[noreturn]] virtual void ThreadMain() = 0;
+  };
+
+  bool Create(Delegate *delegate);
+
+  static int CurrentId();
+
+  static void SetName(const char *);
+
+  static void *thread_handler_wrapper(Delegate *ctx);
+};
+} // namespace base
+
+struct OSThread : base::ThreadInterface, base::ThreadInterface::Delegate {
+  OSThread(const char *name);
+
+  OSThread(const char *name, uint32_t stack_size);
+
+  bool Start();
+};
+
+enum MemoryPermission {
+  kNoAccess,
+  kRead = 1,
+  kWrite = 2,
+  kExecute = 4,
+  kReadWrite = kRead | kWrite,
+  kReadExecute = kRead | kExecute,
+  kReadWriteExecute = kRead | kWrite | kExecute,
+};
+
+class OSMemory {
+public:
+  static int PageSize();
+
+  static void *Allocate(size_t size, MemoryPermission access);
+
+  static void *Allocate(size_t size, MemoryPermission access, void *fixed_addr);
+
+  static bool Free(void *address, size_t size);
+
+  static bool Release(void *address, size_t size);
+
+  static bool SetPermission(void *address, size_t size, MemoryPermission access);
+};
+
+class OSPrint {
+public:
+  static void Print(const char *format, ...);
+
+  static void VPrint(const char *format, va_list args);
+};
